33182d0e2d6f964fa55f1cca9046179207f8fc98
root <root@DESKTOP-BRJVJTM.localdomain>
2022-11-03 16:05:16 +0300

    edit

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -5,7 +5,7 @@ from fabric.api import env
 from fabric.api import put
 from fabric.api import run
 
-env.hosts = ["104.196.50.234", "35.237.11.53"]
+env.hosts = ["3.238.87.137", "54.166.57.53"]
 
 
 def do_deploy(archive_path):
